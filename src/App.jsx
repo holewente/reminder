a75fb732de4ca6898ff7e20522156b327c6a4e36
Your Name <you@example.com>
2022-10-05 14:07:06 +0200

    modal+delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {pers} from './data';
+import {useState} from 'react'
+import {List} from './components/List'
 
 
 
 function App() {
   const [people,setPeople]=useState(pers)
+
   return (
-    <div className='App'>
-      <h4>Todays birthdays</h4>
+    <div className='container border text-center'>
+      <h4><u>Todays birthdays</u> </h4>
       <List people={people}/>
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,16 +5,45 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { motion } from "framer-motion";
+import Modal from 'react-modal';
+import {List} from './components/List'
 
-
-export const List=({people})=>{
+export const List=({people,setPeople})=>{
+  const [modalIsOpen,setIsOpen]=useState(false)
+  const [imgUrl,setImgUrl]=useState('');
+  const openModal=(e)=>{
+    console.log(e.traget)
+    setImgUrl(e.target.src)
+    setIsOpen(true)
+  };
+  const closeModale=()=>{
+    setIsOpen(false);
+  }
+  const handleDelete=(e)=>{
+    console.log(e.target.id)
+  }
     console.log(people);
+
+    const customStyles = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+      },
+    };
+    
+
     return(
         <>
         <div className="row justify-content-center text-center">
+          {people.length==0 && <div>...no events</div>}
         {people.map((obj)=>(
             <div className="mt-2 col-md-4 col-12">
-            <Card sx={{ maxWidth: 345 }} className="border border-danger" >
+            <Card sx={{ maxWidth: 280 }} className="border border-danger" >
             <CardMedia
               component="img"
               height="140"
@@ -30,14 +59,30 @@ export const List=({people})=>{
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
+              <motion.span whileHover={{ scale: 1.3 }}>
+
+              
+              <Button id={obj.id} size="small" color="primary" onClick={handleDelete}>
+                Delete
+              </Button>
+              </motion.span>
             </CardActions>
           </Card>
           </div>
         ))}
         
         </div>
+        <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <button onClick={closeModal}>close</button>
+        <Button onClick={closeModal} size="small" color="primary">close</Button>
+        <div><img className='modalImg' src={imgUrl} alt="foto" />
+        </div>
+      </Modal>
         
         </>
     )
